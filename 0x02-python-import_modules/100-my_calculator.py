5e78b5a9d7f4683af075701d42224cca04b29077
Godsent Ehiz <111048723+Eh1z@users.noreply.github.com>
2022-12-01 21:27:37 +0100

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,27 +1,19 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
+    """Handle basic arithmetic operations."""
     from calculator_1 import add, sub, mul, div
     import sys
 
-    if len(sys.argv) != 4:
+    if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
+        sys.exit(1)
 
-        a = sys.argv[1]
-        b = sys.argv[3]
-        operator = sys.argv[2]
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(ops.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
 
-        if operator != '+' and operator != '-' \
-           and operator != '*' and operator != '/':
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
-
-        if operator == '+':
-            print("{} + {} = {}".format(int(a), int(b), add(int(a), int(b))))
-        elif operator == '-':
-            print("{} - {} = {}".format(int(a), int(b), sub(int(a), int(b))))
-        elif operator == '*':
-            print("{} * {} = {}".format(int(a), int(b), mul(int(a), int(b))))
-        elif operator == '/':
-            print("{} / {} = {}".format(int(a), int(b), div(int(a), int(b))))
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
