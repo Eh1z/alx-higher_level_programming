9de1aa51e5126a8d06774aeee1b40a19dafb9b9f
Eh1z <godsentehiz11@gmail.com>
2023-03-13 18:42:34 +0000

    looping in multiple languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// script to print strings using a loops
+
+const myString = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for ( i = 0; i < 3; i++ ) {
+  console.log(myString[i]);
+}
